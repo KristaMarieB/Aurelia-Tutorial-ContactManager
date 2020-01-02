500b2ae2ff773e64338a0ccc07dd036a853c7c9f
betcherkm <betcherkm@gmail.com>
2020-01-02 14:04:23 -0800

    About 1/3 of the way through tutorial

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
+import {PLATFORM} from 'aurelia-pal';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+
 export class App {
-  message = 'Hello World!';
+  configureRouter(config, router){
+    config.title = 'Contacts';
+    config.map =([
+      {route: '', moduleId: PLATFORM.moduleName('no-selection'), title: 'Select'},
+      {route: 'contacts/id', moduleId: PLATFORM.moduleName('contact-detail'), name: 'contacts' }
+    ]);
+
+    this.router = router;
+  }
 }
